be1b8fc9fe48f787ce2c4eb1ceca9cbad5afe7ee
niexiaodong <niexiaodong@51hitech.com>
2020-09-27 17:33:03 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# steven-vue-monaco-editor
+# vue-monaco-editor 编辑器组件使用
+
+## 安装
+
+### monaco-editor和monaco-editor-webpack-plugin安装
+
+*  yarn add monaco-editor
+*  yarn add monaco-editor-webpack-plugin
+
+### 配置vue.config.js(vue-cli3.0需在根目录中新建)
+
+	const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+	module.exports = {
+	    configureWebpack: {
+	        plugins: [
+	            new MonacoWebpackPlugin()
+	        ]
+	    }
+	}
+
+## 组件
+
+* monacoEditor
+   * opts 配置项
+      
+		    value: '',//编辑器初始化输入内容
+		    autoIndent:true,//启用自动缩进调整。默认为false。
+		    roundedSelection: true, // 右侧不显示编辑器预览框
+		    autoIndent: true, // 自动缩进
+            language:'Aviator',//语言
+            theme:'myCoolTheme',//主题
+  *  getValue() 手动获取编辑内容
+  *  changeValue(val) 编辑器内容发生改变时触发，val为当前编辑器内容 
+  
+* 自义定语言 
+
+   * 语言高亮token
+     1. monaco.languages.setMonarchTokensProvider(languageId, tokenOpt)
+     2. 配置项参考 ./util/aviator-language
+     
+   * 语言提示器
+   	 1. monaco.registerCompletionItemProvider(languageId, {provideCompletionItems:function}) 
+   	 2. provideCompletionItems 函数返回项 配置参考 ./util/custom-completion	
+      
+* 自定义主题 
+	
+	* monaco.editor.defineTheme('themeId', themeOption);
+	* 配置项参考 ./util/myCoolTheme
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <!--调用子组件-->
+    <monacoEditor ref="monaco" @change="changeValue" :opts="opts" :height="300" :width="800"></monacoEditor>
+  </div>
+</template>
+
+<script>
+import monacoEditor from './components/monacoEditor/'
+
+export default {
+  name: 'app',
+  components: {
+    monacoEditor
+  },
+  data () {
+    return {
+        //配置项
+         opts: {
+          // eslint-disable-next-line
+	        value:['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+          autoIndent:true,//启用自动缩进调整。默认为false。
+          roundedSelection: true, // 右侧不显示编辑器预览框
+          selectOnLineNumbers: true,
+          readOnly: false,
+          cursorStyle: 'line',
+          automaticLayout: false,
+          glyphMargin: true,
+          theme: 'vs-dark'
+        }
+    }
+  },
+      methods: {
+      // 手动获取编辑内容
+      getValue() {
+        this.$message.info(this.$refs.monaco.getVal())
+      },
+      // 编辑内容
+      changeValue(val) {
+        // eslint-disable-next-line no-console
+        console.log(val)
+        this.opts.value = val
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  margin-top: 60px;
+}
+</style>
