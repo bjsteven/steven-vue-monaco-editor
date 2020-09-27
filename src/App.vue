0153535ec6fe079c3001f58d2fbf8368dd28cfeb
niexiaodong <niexiaodong@51hitech.com>
2020-09-27 17:56:10 +0800

    feat: code format

--- src/App.vue
+++ src/App.vue
@@ -11,37 +11,39 @@ import monacoEditor from './components/monacoEditor/'
 export default {
   name: 'app',
   components: {
-    monacoEditor
+    monacoEditor,
   },
-  data () {
+  data() {
     return {
-        //配置项
-         opts: {
-          // eslint-disable-next-line
-	        value:['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-          autoIndent:true,//启用自动缩进调整。默认为false。
-          roundedSelection: true, // 右侧不显示编辑器预览框
-          selectOnLineNumbers: true,
-          readOnly: false,
-          cursorStyle: 'line',
-          automaticLayout: false,
-          glyphMargin: true,
-          theme: 'vs-dark'
-        }
+      //配置项
+      opts: {
+        // eslint-disable-next-line
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+          '\n'
+        ),
+        autoIndent: true, //启用自动缩进调整。默认为false。
+        roundedSelection: true, // 右侧不显示编辑器预览框
+        selectOnLineNumbers: true,
+        readOnly: false,
+        cursorStyle: 'line',
+        automaticLayout: false,
+        glyphMargin: true,
+        theme: 'vs-dark',
+      },
     }
   },
-      methods: {
-      // 手动获取编辑内容
-      getValue() {
-        this.$message.info(this.$refs.monaco.getVal())
-      },
-      // 编辑内容
-      changeValue(val) {
-        // eslint-disable-next-line no-console
-        console.log(val)
-        this.opts.value = val
-      }
-    }
+  methods: {
+    // 手动获取编辑内容
+    getValue() {
+      this.$message.info(this.$refs.monaco.getVal())
+    },
+    // 编辑内容
+    changeValue(val) {
+      // eslint-disable-next-line no-console
+      console.log(val)
+      this.opts.value = val
+    },
+  },
 }
 </script>
 
